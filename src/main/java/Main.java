092489b84d13eb7382e917da14d1caaab52963c8
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-05-27 12:29:22 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("programa para calcular a velocidade média em km/h");
+    System.out.println("");
+    System.out.println("A distância considerada é");
+    double distancia = 1000;
+      System.out.println(distancia);
+    System.out.println("o tempo gasto é");
+    double tempo = 7;
+    System.out.println(tempo);
+    System.out.println("A velociada média é ");
+    double velocidadeMedia = distancia / tempo;
+
+    System.out.println(velocidadeMedia);
+  
+  }
+}
